643d396751660eaed5b2aca9e0c269babd2375bf
Roberto Leibman <roberto@42lines.net>
2019-12-13 13:22:14 -0800

    Wrote a bunch more documentation

---
+++ server/src/main/scala/api/HTMLService.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2019 Roberto Leibman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package api
+
+import akka.http.scaladsl.server.directives.ContentTypeResolver
+import akka.http.scaladsl.server.{ Directives, Route }
+
+/**
+ * This service serves the site's static content, including the scala.js generated content
+ */
+trait HTMLService extends Directives with Config {
+  val staticContentDir: String = config.getString("full-scala-stack.staticContentDir")
+
+  override def getFromDirectory(
+      directoryName: String
+  )(implicit resolver: ContentTypeResolver): Route =
+    extractUnmatchedPath { unmatchedPath =>
+      getFromFile(s"$staticContentDir/$unmatchedPath")
+    }
+
+  def htmlRoute: Route =
+    pathEndOrSingleSlash {
+      get {
+        getFromFile(s"$staticContentDir/index.html")
+      }
+    } ~
+    get {
+      extractUnmatchedPath { path =>
+        encodeResponse {
+          getFromDirectory(staticContentDir)
+        }
+      }
+    }
+}
